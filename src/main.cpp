5e491434eac3327ead76eb0898c22dce31346783
“Roman Halenko” <owlbro.popping@gmail.com>
2026-02-17 16:26:14 +0200

    refactor: magic numbers, constants naming

--- src/main.cpp
+++ src/main.cpp
@@ -3,30 +3,33 @@
 #define BLUE_LED_PIN 15
 #define RED_LED_PIN 16
 
-const uint8_t short_phase_duration = 70;
-const uint8_t long_phase_duration = 240;
+const uint8_t blink_duration = 70;
+const uint8_t blink_count = 4;
+const uint8_t blinking_phase_repeat = 2;
+const uint8_t constant_duration = 240;
+const uint8_t constant_phase_repeat = 4;
 
 void activate_police_lights() {
-  for (int i = 0; i < 2; i++) {
-    for (int j = 0; j < 4; j++) {
+  for (int i = 0; i < blinking_phase_repeat; i++) {
+    for (int j = 0; j < blink_count; j++) {
       digitalWrite(BLUE_LED_PIN, HIGH);
-      delay(short_phase_duration);
+      delay(blink_duration);
       digitalWrite(BLUE_LED_PIN, LOW);
-      delay(short_phase_duration);
+      delay(blink_duration);
     }
-    for (int j = 0; j < 4; j++) {
+    for (int j = 0; j < blink_count; j++) {
       digitalWrite(RED_LED_PIN, HIGH);
-      delay(short_phase_duration);
+      delay(blink_duration);
       digitalWrite(RED_LED_PIN, LOW);
-      delay(short_phase_duration);
+      delay(blink_duration);
     }
   }
-  for (int i = 0; i < 4; i++) {
+  for (int i = 0; i < constant_phase_repeat; i++) {
     digitalWrite(BLUE_LED_PIN, HIGH);
-    delay(long_phase_duration);
+    delay(constant_duration);
     digitalWrite(BLUE_LED_PIN, LOW);
     digitalWrite(RED_LED_PIN, HIGH);
-    delay(long_phase_duration);
+    delay(constant_duration);
     digitalWrite(RED_LED_PIN, LOW);
   }
 }
