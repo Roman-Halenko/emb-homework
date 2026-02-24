23f188be2040b077a13b95451ec886b5334f6302
Roman Halenko <owlbro.popping@gmail.com>
2026-02-25 00:40:54 +0200

    Initial commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,37 @@
 #include <Arduino.h>
+#include <ESP32Servo.h>
 
 #define ADC_PIN 15
+#define SERVO_PIN 4
 
-const float U_ref = 3.3;
-const float ADC_max = 4095.0;
+Servo servo1;
+
+const int servo1_min = 700;
+const int servo1_max = 2200;
+const int servo1_max_deg = 180;
+const int servo1_freq = 50;
+const int ADC_max = 4095;
+
+int servo1_position = 0;
 
 void setup() {
   Serial.begin(115200);
+
+  ESP32PWM::allocateTimer(0);
+  ESP32PWM::allocateTimer(1);
+  ESP32PWM::allocateTimer(2);
+  ESP32PWM::allocateTimer(3);
+
+  servo1.setPeriodHertz(servo1_freq);
+
   analogReadResolution(12);
 }
 
 void loop() {
   uint16_t raw_val = analogRead(ADC_PIN);
-  float u_calc = (raw_val / ADC_max) * U_ref;
-  float u_measure = analogReadMilliVolts(ADC_PIN) / 1000.0;
-  float error_abs = u_calc - u_measure;
-  float error_percent = (error_abs / u_measure) * 100.0;
 
-  Serial.printf("Raw: %d; U_calc: %.2fV; U_m: %.2fV; Error: %.1f%%\n", raw_val, u_calc, u_measure, error_percent);
-  delay(100);
+  servo1.attach(SERVO_PIN, servo1_min, servo1_max);
+  servo1_position = (raw_val * servo1_max_deg) / ADC_max;
+  servo1.write(servo1_position);
+  delay(10);
 }
