011105f80c81dd9ca305bd38ff9fff37d9965d64
“Roman Halenko” <owlbro.popping@gmail.com>
2026-04-06 18:44:14 +0300

    Refactoring

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
 #include <Arduino.h>
 
 #define FAN_PIN 15 // GPIO pin connected to the fan relay
-// #define VENTILATION_INTERVAL 3600000000 // 1 hour in microseconds
-// #define VENTILATION_DURATION 900000000  // 15 minutes in microseconds
-#define VENTILATION_INTERVAL 3000000 // 3 seconds in microseconds (for testing purposes)
-#define VENTILATION_DURATION 500000  // 0.5 seconds in microseconds (for testing purposes)
+// #define VENTILATION_INTERVAL_US 3600000000ULL // 1 hour in microseconds
+// #define VENTILATION_DURATION_US 900000000ULL  // 15 minutes in microseconds
+#define VENTILATION_INTERVAL_US 3000000ULL // 3 seconds in microseconds (for testing purposes)
+#define VENTILATION_DURATION_US 500000ULL  // 0.5 seconds in microseconds (for testing purposes)
 #define TIMER_DIVIDER 80
 
 enum VentilationState {
@@ -20,11 +20,11 @@ IRAM_ATTR void onTimer() {
   if (state == VENTILATION_ON) {
     state = VENTILATION_OFF;
     digitalWrite(FAN_PIN, LOW);
-    timerAlarmWrite(timer, VENTILATION_INTERVAL - VENTILATION_DURATION, true);
+    timerAlarmWrite(timer, VENTILATION_INTERVAL_US - VENTILATION_DURATION_US, true);
   } else {
     state = VENTILATION_ON;
     digitalWrite(FAN_PIN, HIGH);
-    timerAlarmWrite(timer, VENTILATION_DURATION, false);
+    timerAlarmWrite(timer, VENTILATION_DURATION_US, false);
   }
 }
 
@@ -36,7 +36,7 @@ void setup() {
 
   timer = timerBegin(0, TIMER_DIVIDER, true);
 
-  timerAlarmWrite(timer, VENTILATION_INTERVAL, true);
+  timerAlarmWrite(timer, VENTILATION_INTERVAL_US, true);
   timerAttachInterrupt(timer, &onTimer, true);
   timerAlarmEnable(timer);
 }
