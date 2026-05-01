b9a27d525880e02e292c409c73b0453a53acd86b
Volodymyr Kaliuzhnyi <vladimirvk2007@gmail.com>
2026-05-01 19:56:42 +0200

    Fix SMA
    Signed-off-by: Volodymyr Kaliuzhnyi <vladimirvk2007@gmail.com>

--- lib/SMA/SMA.h
+++ lib/SMA/SMA.h
@@ -1,12 +1,12 @@
 #pragma once
 
-#include <stdio.h>
+#include <stdint.h>
 
 template <size_t N>
 class SMA {
   public:
-    SMA() : _sum(0), _index(0) {
-      for (int i = 0; i < N; ++i) _buffer[i] = 0;
+    SMA() : _sum(0), _index(0), _count(0) {
+      for (size_t i = 0; i < N; ++i) _buffer[i] = 0;
     }
 
     uint16_t process(uint16_t newValue) {
@@ -15,12 +15,16 @@ class SMA {
       _sum += newValue;
 
       _index = (_index + 1) % N;
+      if (_count < N) {
+        _count++;
+      }
 
-      return (uint16_t)(_sum / N);
+      return (uint16_t)(_sum / _count);
     }
 
   private:
     uint16_t _buffer[N];
     uint32_t _sum;
-    int _index;
+    size_t _index;
+    size_t _count;
 };
